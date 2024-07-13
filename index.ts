01c71762e5e9c69c34d20ba12dd45ce249f7aa70
HIBA KHAN <161249784+hibakashifkhan@users.noreply.github.com>
2024-07-13 19:24:47 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+// Write a program that determines whether a given number is positive or negative.
+import inquirer from "inquirer"
+
+let number = await inquirer.prompt([
+    {
+        name :"number",
+        message : "Please Enter a Number.",
+        type : "number"
+
+    },
+])
+ if(number.number >= 0 ){
+    console.log("The number is Positive.");
+ }
+ else if(number.number = 'null' ){
+    console.log("Please Enter a value in positive or negative. It is not a number (NaN)");
+ }
+ else{
+    console.log("The number is Negative.");
+ }
